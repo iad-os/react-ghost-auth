3aa2cab5680228791b129c2b304955551212cfed
nicola.vurchio <nicola.vurchio@iad2.it>
2021-11-12 16:07:54 +0100

    Minor fix

---
+++ src/auth/components/index.ts
@@ -0,0 +1,6 @@
+import AutoLogin from './AutoLogin';
+import LoggedIn from './LoggedIn';
+import LogginIn from './LogginIn';
+import RequireAuth from './RequireAuth';
+
+export { LogginIn, AutoLogin, LoggedIn, RequireAuth };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import AuthenticationProvider, {
   useAuthentication,
 } from './auth/Authentication';
-import LogginIn from './auth/components/LogginIn';
-import AutoLogin from './auth/components/AutoLogin';
-import LoggedIn from './auth/components/LoggedIn';
-import Public from './auth/components/Public';
-import RequireAuth from './auth/components/RequireAuth';
 
 export default AuthenticationProvider;
 
 export type { AuthenticationOptions } from './auth/Authentication';
-
 export { useAuthentication };
-
-export { LogginIn, AutoLogin, LoggedIn, RequireAuth };
