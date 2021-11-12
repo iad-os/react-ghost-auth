d2583e6322b2feaa96fd8b15cd9cdd6f7f089a84
nicola.vurchio <nicola.vurchio@iad2.it>
2021-11-12 16:10:58 +0100

    Add Public component

--- src/auth/components/index.ts
+++
@@ -1,6 +0,0 @@
-import AutoLogin from './AutoLogin';
-import LoggedIn from './LoggedIn';
-import LogginIn from './LogginIn';
-import RequireAuth from './RequireAuth';
-
-export { LogginIn, AutoLogin, LoggedIn, RequireAuth };
