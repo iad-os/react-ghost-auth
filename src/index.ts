098a3c7f381a8d1c64e038603b3ef5d2790b3a09
nicola.vurchio <nicola.vurchio@iad2.it>
2021-11-17 17:03:35 +0100

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import AuthenticationProvider from './Authentication';
 import { useAuthentication, AuthenticationConfig } from './Authentication';
-
+import Autologin from './components/AutoLogin';
+import LoggedIn from './components/LoggedIn';
+import LogginIn from './components/LogginIn';
+import Public from './components/Public';
+import RequireAuth from './components/RequireAuth';
+import { TokenResponse } from './models/TokenResponse';
 export default AuthenticationProvider;
 
 export { useAuthentication };
-export type { AuthenticationConfig };
+export type { AuthenticationConfig, TokenResponse };
+
+export { Autologin, LoggedIn, LogginIn, Public, RequireAuth };
