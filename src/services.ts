4e1f1b58b8abbee552858621dfa8f0046ba9f2c0
Nicola Vurchio <nicola.vurchio@iad2.it>
2026-02-05 15:24:59 +0100

    Refactor getWellKnown service to simplify fetch request by removing unnecessary headers and update response handling to parse text as JSON.

--- src/services.ts
+++ src/services.ts
@@ -2,14 +2,10 @@ import { WellKnown } from './models';
 
 export async function getWellKnown(issuer: string): Promise<WellKnown> {
   const wellKnownUrl = `${issuer}/.well-known/openid-configuration`;
-  const response = await fetch(wellKnownUrl, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const response = await fetch(wellKnownUrl);
   if (!response.ok) {
     throw new Error(`Failed to fetch well-known: ${wellKnownUrl}`);
   }
-  const data = await response.json();
-  return data as WellKnown;
+  const data = await response.text();
+  return JSON.parse(data) as WellKnown;
 }
